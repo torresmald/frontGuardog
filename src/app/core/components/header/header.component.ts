0521c60cfd5169a135681de778c2df531325ae79
torresmald <jonathan.torresmald@gmail.com>
2023-10-31 16:15:51 +0100

    started register styles

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,19 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public isMobile: boolean = false;
+  public isLightMode: boolean = true;
+  public theme?: string;
+  public language: string = 'es';
 
-  public openMenuMobile(){
-    this.isMobile = !this.isMobile    
+  ngOnInit(): void {
+    const userPrefersDark =
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPrefersLight =
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: light)').matches;
+    if (userPrefersDark) {
+      this.theme = 'dark';
+    }
+    if (userPrefersLight) {
+      this.theme = 'light';
+    }
+  }
+
+  public openMenuMobile() {
+    this.isMobile = !this.isMobile;
+  }
+
+  public onChangeTheme() {
+    this.isLightMode = !this.isLightMode;
+    console.log(this.isLightMode);
+  }
+  public onChangeLanguage() {
+    if (this.language === 'es') {
+      this.language = 'uk'
+    } else {
+      this.language = 'es'
+    }
+    console.log(this.language);
   }
 }
-
-
-
-
-
