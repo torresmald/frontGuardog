dbcdd294b6a62ae58da0a01e5d3ed96763c68093
torresmald <jonathan.torresmald@gmail.com>
2023-10-31 09:48:22 +0100

    started styles home

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,18 +5,15 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { LoadingService } from './services/Loading/loading.service';
 import { LoaderComponent } from './components/loader/loader.component';
 
+
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, LoaderComponent],
-  imports: [
-    CommonModule,
-    CoreRoutingModule,
-    HttpClientModule
-  ],
+  imports: [CommonModule, CoreRoutingModule, HttpClientModule],
   exports: [HeaderComponent, FooterComponent, LoaderComponent],
-  providers: [LoadingService]
+  providers: [LoadingService],
 })
-export class CoreModule { }
+export class CoreModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,17 @@ module.exports = {
       },
       fontFamily: {
         'dog': ['dog', 'sans-serif']
-      }
+      },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
