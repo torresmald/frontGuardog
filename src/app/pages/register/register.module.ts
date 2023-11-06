9aec80efae09314a10270ab415c5901fc7548c39
torresmald <jonathan.torresmald@gmail.com>
2023-11-06 14:07:48 +0100

    fixed about

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -7,7 +7,10 @@ import { FormModule } from '@coreui/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {MatSliderModule} from '@angular/material/slider';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 
 
 @NgModule({
@@ -21,7 +24,11 @@ import {MatSliderModule} from '@angular/material/slider';
     FormModule,
     ReactiveFormsModule,
     SharedModule,
-    MatSliderModule
+    MatSliderModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatFormFieldModule
   ]
 })
 export class RegisterModule { }
