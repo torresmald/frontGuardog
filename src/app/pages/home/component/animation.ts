02f1cc7d6347cc61a8f3164798777a7161915142
jerry <jerrygonzalezzambrano@gmail.com>
2023-11-11 17:22:20 +0100

    animaciones y logos

---
+++ src/app/pages/home/component/animation.ts
@@ -0,0 +1,63 @@
+export class homeAnimation {
+  public animation1?: boolean;
+  public animation2?: boolean;
+  public animation3?: boolean;
+  public animation4?: boolean;
+
+  constructor({
+    animation1 = false,
+    animation2 = false,
+    animation3 = false,
+    animation4 = false,
+  }: {
+    animation1?: boolean;
+    animation2?: boolean;
+    animation3?: boolean;
+    animation4?: boolean;
+  }) {
+    this.animation1 = animation1;
+    this.animation2 = animation2;
+    this.animation3 = animation3;
+    this.animation4 = animation4;
+  }
+}
+
+export class ScrollListener {
+  static listen(callback: (booleans: object) => void): void {
+    let animationHome = new homeAnimation({});
+    const onScroll = () => {
+      const windowHeight = window.innerHeight;
+      const pageHeight = document.documentElement.scrollHeight;
+      const scrollPosition = window.scrollY;
+      const scrollPercentage =
+        (scrollPosition / (pageHeight - windowHeight)) * 100;
+
+      switch (true) {
+        case scrollPercentage >= 10 && scrollPercentage < 36:
+          animationHome.animation1 = true;
+          break;
+
+        case scrollPercentage >= 36 && scrollPercentage < 61:
+          animationHome.animation2 = true;
+          break;
+
+        case scrollPercentage >= 61 && scrollPercentage < 92:
+          animationHome.animation3 = true;
+          break;
+
+        case scrollPercentage >= 92 && scrollPercentage <= 101:
+          animationHome.animation4 = true;
+          break;
+
+        default:
+          animationHome = new homeAnimation({});
+          break;
+      }
+
+      callback(animationHome);
+      console.log(scrollPercentage);
+      
+    };
+    window.addEventListener('scroll', onScroll);
+  }
+}
