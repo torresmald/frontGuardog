335e245a950e2deddd4e64981d7c52745a6d916d
jgonzalez <jerrygonzalezzambrano@gmail.com>
2023-11-27 21:34:42 +0100

    Limpieza de codigo basura

--- src/app/pages/parent-view/service/service.component.ts
+++ src/app/pages/parent-view/service/service.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Services } from 'src/app/core/models/Services/transformed/ServiceModel';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
-import { ServicesService } from 'src/app/core/services/Services/servicesService.service';
 import {CourierService} from "../../../core/services/courier/courier.service";
 
 registerLocaleData(localeEs, 'es');
