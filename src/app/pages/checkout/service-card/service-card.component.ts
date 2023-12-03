5a183846024c128df58ea8d67d3e69f9a6f7b433
jgonzalez <jerrygonzalezzambrano@gmail.com>
2023-12-03 01:03:00 +0100

    envio de servicios al backend preparado y imagenes de mascotas asociadas a cada servicio ya se muestran en la vista checkout

---
+++ src/app/pages/checkout/service-card/service-card.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Services} from "../../../core/models/Services/transformed/ServiceModel";
+import {PetsService} from "../../../core/services/Pets/petsService.service";
+import {Pets} from "../../../core/models/Pets/transformed/PetModel";
+
+@Component({
+  selector: 'app-service-card',
+  templateUrl: './service-card.component.html',
+  styleUrls: ['./service-card.component.scss']
+})
+export class ServiceCardComponent implements OnInit{
+  @Input() service?: Services
+  public pet?: Pets
+
+  constructor(private petsService: PetsService,) {
+  }
+  ngOnInit() {
+    if (this.service?.petId)
+      this.petsService.getPet(this.service.petId).subscribe(value => this.pet = value)
+  }
+}
