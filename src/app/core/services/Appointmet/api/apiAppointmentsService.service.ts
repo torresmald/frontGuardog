dcaf1fbc6467990cd0e073c494208300f709f078
Jonathan Torres <jonathan@actycrea.com>
2024-04-04 22:58:07 +0200

    added environments variables

--- src/app/core/services/Appointmet/api/apiAppointmentsService.service.ts
+++ src/app/core/services/Appointmet/api/apiAppointmentsService.service.ts
@@ -3,10 +3,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiAppointments } from 'src/app/core/models/Appointments/api/apiAppointmentModel';
 import { ApiServices } from 'src/app/core/models/Services/api/apiServiceModel';
+import { environment } from 'src/environments/environment';
 
-//TODO VARIABLES DE ENTORNO
 
-const URL_API = 'https://api-guardog.vercel.app/appointments';
+const URL_API = {
+  DOMAIN: environment.baseUrl,
+  APPOINTMENTS: '/appointments',
+  USER: '/user',
+  DATE: '/date?date='
+}
 
 @Injectable({
   providedIn: 'root',
@@ -15,27 +20,27 @@ export class ApiAppointmentsService {
   constructor(private http: HttpClient) {}
 
   public getApiAppointments(): Observable<ApiAppointments[]> {
-    return this.http.get<ApiAppointments[]>(URL_API);
+    return this.http.get<ApiAppointments[]>(`${URL_API.DOMAIN}${URL_API.APPOINTMENTS}`);
   }
   public getApiAppointment(id: string): Observable<ApiAppointments> {
-    return this.http.get<ApiAppointments>(`${URL_API}/${id}`);
+    return this.http.get<ApiAppointments>(`${URL_API.DOMAIN}${URL_API.APPOINTMENTS}/${id}`);
   }
 
   public getApiAppointmentsUser(user: string): Observable<ApiAppointments[]> {
-    return this.http.get<ApiAppointments[]>(`${URL_API}/user/${user}`);
+    return this.http.get<ApiAppointments[]>(`${URL_API.DOMAIN}${URL_API.APPOINTMENTS}${URL_API.USER}/${user}`);
   }
 
   public getApiAppointmentsDate(date: string): Observable<ApiServices[]> {
-    return this.http.get<ApiServices[]>(`${URL_API}/date?date=${date}`);
+    return this.http.get<ApiServices[]>(`${URL_API.DOMAIN}${URL_API.APPOINTMENTS}${URL_API.DATE}${date}`);
   }
 
   public registerApiAppointment(
     body: ApiAppointments
   ): Observable<ApiAppointments> {
-    return this.http.post<ApiAppointments>(URL_API, body);
+    return this.http.post<ApiAppointments>(`${URL_API.DOMAIN}${URL_API.APPOINTMENTS}`, body);
   }
 
   public deleteApiAppointment(id: string): Observable<string> {
-    return this.http.delete<string>(`${URL_API}/${id}`);
+    return this.http.delete<string>(`${URL_API.DOMAIN}${URL_API.APPOINTMENTS}/${id}`);
   }
 }
--- src/app/core/services/Parents/api/apiParentsService.service.ts
+++ src/app/core/services/Parents/api/apiParentsService.service.ts
@@ -3,9 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiParents } from 'src/app/core/models/Parents/api/apiParentModel';
 import { ApiUsers } from 'src/app/core/models/Users/api/apiUserModel';
+import { environment } from 'src/environments/environment';
 
-const URL_API = 'https://api-guardog.vercel.app/parents';
-
+const URL_API = {
+  DOMAIN: environment.baseUrl,
+  PARENTS: '/parents',
+  REGISTER: '/register',
+  LOGIN: '/login',
+  VERIFY: '/verify',
+  FORGOT: '/forgot-password'
+}
 
 @Injectable({
   providedIn: 'root'
@@ -15,38 +22,38 @@ export class ApiParentsService {
   constructor(private http: HttpClient) { }
 
   public getApiParents(): Observable<ApiParents[]>{
-    return this.http.get<ApiParents[]>(URL_API)
+    return this.http.get<ApiParents[]>(`${URL_API.DOMAIN}${URL_API.PARENTS}`)
   }
 
   public getApiParent(id: string): Observable<ApiParents>{
-    return this.http.get<ApiParents>(`${URL_API}/${id}`)
+    return this.http.get<ApiParents>(`${URL_API.DOMAIN}${URL_API.PARENTS}/${id}`)
   }
 
   public registerApiParent (body: FormData): Observable<ApiParents>{
-    return this.http.post<ApiParents>(`${URL_API}/register`, body)
+    return this.http.post<ApiParents>(`${URL_API.DOMAIN}${URL_API.PARENTS}${URL_API.REGISTER}`, body)
   }
 
   public loginApiParent (body: ApiParents): Observable<ApiUsers>{
-    return this.http.post<ApiUsers>(`${URL_API}/login`, body)
+    return this.http.post<ApiUsers>(`${URL_API.DOMAIN}${URL_API.PARENTS}${URL_API.LOGIN}`, body)
   }
   
   public verifyApiParentAccount(token: string): Observable<string>{
-    return this.http.get<string>(`${URL_API}/verify/${token}`)
+    return this.http.get<string>(`${URL_API.DOMAIN}${URL_API.PARENTS}${URL_API.VERIFY}/${token}`)
   }
 
   public forgotApiPassword(body: string): Observable<string>{
-    return this.http.post<string>(`${URL_API}/forgot-password`, body)
+    return this.http.post<string>(`${URL_API.DOMAIN}${URL_API.PARENTS}${URL_API.FORGOT}`, body)
   }
   public forgotApiPasswordToken(token:string): Observable<string>{
-    return this.http.get<string>(`${URL_API}/forgot-password/${token}`)
+    return this.http.get<string>(`${URL_API.DOMAIN}${URL_API.PARENTS}${URL_API.FORGOT}/${token}`)
   }
   public updateApiPassword(token:string, password: string): Observable<string>{
     const body = { password }
-    return this.http.post<string>(`${URL_API}/forgot-password/${token}`, body)
+    return this.http.post<string>(`${URL_API.DOMAIN}${URL_API.PARENTS}${URL_API.FORGOT}/${token}`, body)
   }
 
   public updateApiDataParent(id: string, body: string): Observable<string>{
-    return this.http.put<string>(`${URL_API}/${id}`, body)
+    return this.http.put<string>(`${URL_API.DOMAIN}${URL_API.PARENTS}/${id}`, body)
   }
   
 }
