b7e0f2422a3e6f0067bbea2dd6f7c73b67d01721
torresmald <jonathan.torresmald@gmail.com>
2024-04-08 11:04:59 +0200

    all input same styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: []
 })
 export class AppComponent {
   public title: string = 'Guardog';
