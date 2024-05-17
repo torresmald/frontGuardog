16ea4c8f0c81860902a7ccbca8fc774777ac2ac2
Jonathan Torres <jonathan@actycrea.com>
2024-05-17 17:02:54 +0200

    maps component

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-
-    export const environment = {
-        apiMapboxKey: "pk.eyJ1IjoidG9ycmVzbWFsZCIsImEiOiJjbHZkZzJndmYwbWd2MmlvNjk4cXN3cGZhIn0.bkyGeUch-OYN50jgzdG8ag",
-        baseUrl: "https://apiguardog.onrender.com"
-    }
